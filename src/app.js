adc41b293f75ba81f7ded5aef9142229710d50d7
yui0722 <takahaimasato222@gmail.com>
2016-09-13 13:35:05 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,145 @@
+var itemsLayer;
+var cart;
+var xSpeed = 0; //カートの移動速度
+
+var detectedX;　 //現在タッチしているX座標 end
+var savedX;　 //前回タッチしていたX座標 orizin
+var touching = false;　 //タッチ状況管理用flag
+
+var gameScene = cc.Scene.extend({
+  onEnter: function() {
+    this._super();
+    gameLayer = new game();
+    gameLayer.init();
+    this.addChild(gameLayer);
+  }
+});
+
+var game = cc.Layer.extend({
+  init: function() {
+    this._super();
+    //グラデーション背景
+    //  var backgroundLayer = cc.LayerGradient.create(cc.color(0,0,0,255), cc.color(0x46,0x82,0xB4,255));
+
+    //森の背景
+    var background = new cc.Sprite(res.background_png);
+    var size = cc.director.getWinSize();
+    background.setPosition(cc.p(size.width / 2.0, size.height / 2.0));
+    var backgroundLayer = cc.Layer.create();
+    backgroundLayer.addChild(background);
+    this.addChild(backgroundLayer);
+
+    //アイテムがおちてくるレイヤー
+    itemsLayer = cc.Layer.create();
+    this.addChild(itemsLayer);
+
+    //ショッピングカートを操作するレイヤー
+    topLayer = cc.Layer.create();
+    this.addChild(topLayer);
+    cart = cc.Sprite.create(res.cart_png);
+    topLayer.addChild(cart, 0);
+    cart.setPosition(240, 24);
+    this.schedule(this.addItem, 1);
+    //タッチイベントのリスナー追加
+    cc.eventManager.addListener(touchListener, this);
+    //カートの移動のため　Update関数を1/60秒ごと実行させる　
+    this.scheduleUpdate();
+  },
+  addItem: function() {
+    var item = new Item();
+    itemsLayer.addChild(item, 1);
+  },
+  removeItem: function(item) {
+    itemsLayer.removeChild(item);
+  },
+  //カートの移動のため　Update関数を1/60秒ごと実行させる関数
+  update: function(dt) {
+    if (touching) {
+      //現在タッチしているX座標と前回の座標の差分をとる
+      var deltaX = savedX - detectedX;
+                    //保管場所　－　現在置
+      //差分でカートが右にいくか左にいくかを判断する
+      if (deltaX > 0) {
+        xSpeed = -2;
+      }
+      if (deltaX < 0) {
+        xSpeed = 2;
+      }
+      //saveXに今回のX座標を代入し、onTouchMovedイベントで
+      //detectedX変数が更新されても対応できるようにする
+      savedX = detectedX;//移動中にタップの更新をしている
+      if (xSpeed > 0) {
+        cart.setFlippedX(true);
+      }
+      if (xSpeed < 0) {
+        cart.setFlippedX(false);
+      }
+      cart.setPosition(cart.getPosition().x + xSpeed, cart.getPosition().y);
+    }
+  }
+
+});
+
+var Item = cc.Sprite.extend({
+  ctor: function() {
+    this._super();
+    //ランダムに爆弾と果物を生成する
+    if (Math.random() < 0.5) {
+      this.initWithFile(res.bomb_png);
+      this.isBomb = true;
+    } else {
+      this.initWithFile(res.strawberry_png);
+      this.isBomb = false;
+    }
+  },
+  //アイテムが生成された後、描画されるときに実行
+  onEnter: function() {
+    this._super();
+    //ランダムな位置に
+    this.setPosition(Math.random() * 400 + 40, 350);
+    //ランダムな座標に移動させる
+    var moveAction = cc.MoveTo.create(8, new cc.Point(Math.random() * 400 + 40, -50));
+    this.runAction(moveAction);
+    this.scheduleUpdate();
+  },
+  update: function(dt) {
+    //果物の処理　座標をチェックしてカートの接近したら
+    if (this.getPosition().y < 35 && this.getPosition().y > 30 &&
+      Math.abs(this.getPosition().x - cart.getPosition().x) < 10 && !this.isBomb) {
+      gameLayer.removeItem(this);
+      console.log("FRUIT");
+    }
+    //爆弾の処理　座標をチェックしてカートの接近したら　フルーツより爆弾に当たりやすくしている
+    if (this.getPosition().y < 35 && Math.abs(this.getPosition().x - cart.getPosition().x) < 25 &&
+      this.isBomb) {
+      gameLayer.removeItem(this);
+      console.log("BOMB");
+    }
+    //地面に落ちたアイテムは消去
+    if (this.getPosition().y < -30) {
+      gameLayer.removeItem(this)
+    }
+  }
+});
+
+//バーチャルアナログパッド用のタッチリスナーの実装
+var touchListener = cc.EventListener.create({
+  event: cc.EventListener.TOUCH_ONE_BY_ONE,
+  swallowTouches: true,
+  onTouchBegan: function(touch, event) {
+    touching = true;
+    //現在タッチ中のX座標を保持する変数へ代入
+    detectedX = touch.getLocation().x;
+    //前回タッチしていたX座標として代入
+    savedX = detectedX;
+    return true;
+  },
+  onTouchMoved: function(touch, event) {
+    //現在タッチ中のX座標を保持する変数へ代入
+    detectedX = touch.getLocation().x;
+  },
+  onTouchEnded: function(touch, event) {
+    //タッチflagをOFF
+    touching = false;
+  }
+})
---
+++ src/resource.js
@@ -0,0 +1,15 @@
+var res = {
+  bomb_png: "res/bomb.png",
+  cart_png: "res/cart.png",
+  strawberry_png: "res/strawberry.png",
+//  leftbutton_png: "res/leftbutton.png",
+//  rightbutton_png: "res/rightbutton.png",
+  background_png: "res/background.png",
+//  touchorigin_png: "res/touchorigin.png",
+//  touchend_png: "res/touchend.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+  g_resources.push(res[i]);
+}
